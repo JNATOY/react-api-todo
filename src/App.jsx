584599b1b4279418e68f35016ed04d6b40e8462d
JNATOY <jcnatoyepez@gmail.com>
2023-08-11 22:20:28 -0500

    deploy II

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Table, Button, Form } from 'react-bootstrap'
+import {Container,Table,Button,Form} from 'react-bootstrap'
 import axios from 'axios'
+import {API_URL} from './enviroment'
 
-class App extends React.Component {
+class App extends React.Component{
 
-  constructor(props) {
+  constructor(props){
     super(props)
     this.state = (
       {
-        tareas: [],
-        descripcion: '',
-        estado: 'pendiente',
-        id: 0,
-        pos: null
+        tareas : [],
+        descripcion:'',
+        estado:'pendiente',
+        id:0,
+        pos:null
       }
     )
     this.cambioDescripcion = this.cambioDescripcion.bind(this)
@@ -23,112 +24,112 @@ class App extends React.Component {
     this.cambiarEstado = this.cambiarEstado.bind(this)
   }
 
-  componentDidMount() {
+  componentDidMount(){
     console.log("cargando tareas...")
-    axios.get('http://localhost:5000/tarea')
-      .then(res => {
-        console.log(res.data)
-        this.setState({
-          tareas: res.data.content
-        })
+    axios.get(`${API_URL}/tarea`)
+    .then(res=>{
+      console.log(res.data)
+      this.setState({
+        tareas : res.data.content
       })
-  }
-
-  cambioDescripcion(e) {
-    this.setState({
-      descripcion: e.target.value
     })
   }
 
-  mostrar(cod, index) {
-    axios.get('http://localhost:5000/tarea/' + cod)
-      .then(res => {
-        console.log(res.data.content)
-        this.setState({
-          descripcion: res.data.content.descripcion,
-          id: res.data.content.id,
-          pos: index
-        })
-      })
+  cambioDescripcion(e){
+    this.setState({
+      descripcion : e.target.value
+    })
   }
 
-  guardar(e) {
+  mostrar(cod,index){
+    axios.get(`${API_URL}/tarea/`+cod)
+    .then(res=>{
+      console.log(res.data.content)
+      this.setState({
+        descripcion:res.data.content.descripcion,
+        id:res.data.content.id,
+        pos:index
+      })
+    })
+  }
+
+  guardar(e){
     e.preventDefault()
     let cod = this.state.id
     const data = {
-      descripcion: this.state.descripcion,
-      estado: this.state.estado
+      descripcion : this.state.descripcion,
+      estado : this.state.estado
     }
-    console.log("enviando data al servidor :", data)
-
-    if (cod > 0) {
+    console.log("enviando data al servidor :",data)
+    
+    if(cod>0){
       //actualizar
-      axios.put('http://localhost:5000/tarea/' + cod, data)
-        .then(res => {
-          let index_tarea = this.state.pos
-          //this.state.tareas[index_tarea] = res.data.content
-          var temp = this.state.tareas
-          temp[index_tarea] = res.data.content
-          this.setState({
-            tareas: temp,
-            descripcion: '',
-            pos: null,
-            id: 0
-          })
-        })
-    }
-    else {
-      //insertar
-      axios.post('http://localhost:5000/tarea', data)
-        .then(res => {
-          console.log("respuesta del servidor :", res.data.content)
-          this.state.tareas.push(res.data.content)
-          var temp = this.state.tareas
-          this.setState({
-            tareas: temp,
-            descripcion: '',
-            id: 0,
-            pos: null
-          })
-        })
-    }
-
-  }
-
-  eliminar(cod) {
-    axios.delete('http://localhost:5000/tarea/' + cod)
-      .then(res => {
-        console.log(res.data.content)
-        var temp = this.state.tareas.filter((tarea) => tarea.id !== res.data.content.id)
-        this.setState({
-          tareas: temp
-        })
-      })
-  }
-
-  cambiarEstado(cod, pos) {
-    const data = {
-      estado: 'completado'
-    }
-    axios.patch('http://localhost:5000/tarea/' + cod, data)
-      .then(res => {
+      axios.put(`${API_URL}/tarea/`+cod,data)
+      .then(res=>{
+        let index_tarea = this.state.pos
+        //this.state.tareas[index_tarea] = res.data.content
         var temp = this.state.tareas
-        temp[pos] = res.data.content
+        temp[index_tarea] = res.data.content
         this.setState({
-          tareas: temp
+          tareas : temp,
+          descripcion:'',
+          pos:null,
+          id:0
         })
       })
+    }
+    else{
+      //insertar
+      axios.post(`${API_URL}/tarea`,data)
+      .then(res=>{
+        console.log("respuesta del servidor :",res.data.content)
+        this.state.tareas.push(res.data.content)
+        var temp = this.state.tareas
+        this.setState({
+          tareas :temp,
+          descripcion:'',
+          id:0,
+          pos:null
+        })
+      })
+    }
+    
   }
 
-  render() {
-    return (
+  eliminar(cod){
+    axios.delete(`${API_URL}/tarea/`+cod)
+    .then(res=>{
+      console.log(res.data.content)
+      var temp = this.state.tareas.filter((tarea)=>tarea.id !== res.data.content.id)
+      this.setState({
+        tareas:temp
+      })
+    })
+  }
+
+  cambiarEstado(cod,pos){
+    const data = {
+      estado : 'completado'
+    }
+    axios.patch(`${API_URL}/tarea/`+cod,data)
+    .then(res=>{
+      var temp = this.state.tareas
+      temp[pos] = res.data.content
+      this.setState({
+        tareas:temp
+      })
+    })
+  }
+
+  render(){
+    return(
       <div>
         <Container>
           <h1>Lista de Tareas</h1>
           <Form onSubmit={this.guardar}>
             <Form.Group className="mb-3">
               <Form.Control type="text" value={this.state.descripcion}
-                onChange={this.cambioDescripcion}
+              onChange={this.cambioDescripcion}
               />
             </Form.Group>
             <Button variant="primary" type="submit">
@@ -145,36 +146,36 @@ class App extends React.Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.tareas.map((tarea, index) => {
-                return (
+              {this.state.tareas.map((tarea,index)=>{
+                return(
                   <tr key={tarea.id}>
                     <td>{tarea.id}</td>
                     <td>{tarea.descripcion}</td>
                     <td>
-                      <div className="form-check form-switch">
-                        {tarea.estado == 'completado'
-                          ?
-                          <input className="form-check-input" type="checkbox" checked></input>
-                          :
-                          <input className="form-check-input" type="checkbox"></input>
-                        }
-                      </div>
+                        <div className="form-check form-switch">
+                          {tarea.estado == 'completado'
+                            ?
+                            <input className="form-check-input" type="checkbox" checked></input>
+                            :
+                            <input className="form-check-input" type="checkbox"></input>
+                          }
+                        </div>
                     </td>
                     <td>
-                      <Button variant="success" onClick={() => this.mostrar(tarea.id, index)}>
+                      <Button variant="success" onClick={()=>this.mostrar(tarea.id,index)}>
                         Editar
                       </Button>
-                      <Button variant="warning" onClick={() => this.cambiarEstado(tarea.id, index)}>
+                      <Button variant="warning" onClick={()=>this.cambiarEstado(tarea.id,index)}>
                         Completar
                       </Button>
-                      <Button variant="danger" onClick={() => this.eliminar(tarea.id)}>
+                      <Button variant="danger" onClick={()=>this.eliminar(tarea.id)}>
                         Eliminar
                       </Button>
                     </td>
                   </tr>
                 )
               })}
-
+              
             </tbody>
           </Table>
         </Container>
